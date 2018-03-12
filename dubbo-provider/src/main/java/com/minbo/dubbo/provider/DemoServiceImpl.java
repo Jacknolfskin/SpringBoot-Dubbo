deaa77170698365837b9eb5a5a15eab1578081f3
Jacknolfskin <1352490133@qq.com>
2018-03-12 18:14:10 +0800

    新建项目

--- dubbo-provider/src/main/java/com/minbo/dubbo/provider/DemoServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.minbo.dubbo.provider;
-
-public class DemoServiceImpl implements DemoService{
-
-	public String sayHello(String name) {
-		return "Welcome to Minbo's Dubbo, Hello " + name;
-	}
-
-}
